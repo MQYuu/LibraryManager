505877d0a107a8ae21bedffeba4b7b1ef7703e7c
MinhQuang-dev <phanquang704@gmail.com>
2024-11-19 00:32:17 +0700

    update source code

--- library/src/bookdata/Textbook.java
+++
@@ -1,9 +0,0 @@
-package bookdata;
-public class Textbook extends Book {
-    private String condition; // "new" or "used"
-
-    public Textbook(String bookId, String dateAdded, double unitPrice, int quantity, String publisher, String condition) {
-        super(bookId, dateAdded, unitPrice, quantity, publisher);
-        this.condition = condition;
-    }
-}
